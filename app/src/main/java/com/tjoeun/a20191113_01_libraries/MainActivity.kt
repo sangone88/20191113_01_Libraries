5007a1dbb7db7fcc0933b0ad4f5b18ebf576aac0
sangone88 <llil1@naver.com>
2019-11-13 20:38:53 +0900

    Tedpermission을 통한 권한 획득 예제

--- app/src/main/java/com/tjoeun/a20191113_01_libraries/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191113_01_libraries/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.tjoeun.a20191113_01_libraries
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -16,6 +22,30 @@ class MainActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        callBtn.setOnClickListener {
+
+            var permissionListener = object : PermissionListener {
+                override fun onPermissionGranted() {
+                    var uri = Uri.parse("tel:${phoneNumtxt.text.toString().replace("-","")}")
+                    var intent = Intent(Intent.ACTION_CALL, uri)
+                    startActivity(intent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "권한을 설정해야 전화 사용이 가능합니다.", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            TedPermission.with(mContext)
+                .setPermissionListener(permissionListener)
+                .setRationaleMessage("전화 권한을 설정해야 바로 연결 가능합니다.")
+                .setDeniedMessage("설정 > 권한에서 허용 가능합니다.")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+
+        }
+
     }
 
     override fun setValues() {
@@ -23,6 +53,8 @@ class MainActivity : BaseActivity() {
         var url = "https://pbs.twimg.com/profile_images/1153500695506460672/U9lK1j6K_400x400.jpg"
 
         Glide.with(mContext).load(url).into(profileImg)
+
+
 
     }
 
