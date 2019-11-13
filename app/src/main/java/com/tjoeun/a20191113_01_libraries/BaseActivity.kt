aee90cc64afc533607b09bc3ba4047754b3f95b0
sangone88 <llil1@naver.com>
2019-11-13 19:42:26 +0900

    써클 이미지 뷰 사용 예시

--- app/src/main/java/com/tjoeun/a20191113_01_libraries/BaseActivity.kt
+++ app/src/main/java/com/tjoeun/a20191113_01_libraries/BaseActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 abstract class BaseActivity : AppCompatActivity() {
 
     var mContext:Context = this
-    
+
     abstract fun setupEvents()
     abstract fun setValues()
 }
