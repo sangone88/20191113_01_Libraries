e0a8557ae8e5a1ef7aaa8dab67f41c9155da1295
sangone88 <llil1@naver.com>
2019-11-13 19:39:29 +0900

    Initial commit

---
+++ app/src/main/java/com/tjoeun/a20191113_01_libraries/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.tjoeun.a20191113_01_libraries
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
