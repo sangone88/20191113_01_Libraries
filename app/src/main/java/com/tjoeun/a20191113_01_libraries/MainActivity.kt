117c9597098041c0a659f5ac74ba64228944d916
sangone88 <llil1@naver.com>
2019-11-13 19:50:55 +0900

    글라이드를 이용하여 이미지 첨부

--- app/src/main/java/com/tjoeun/a20191113_01_libraries/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191113_01_libraries/MainActivity.kt
@@ -2,6 +2,8 @@ package com.tjoeun.a20191113_01_libraries
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +19,10 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        var url = "https://pbs.twimg.com/profile_images/1153500695506460672/U9lK1j6K_400x400.jpg"
+
+        Glide.with(mContext).load(url).into(profileImg)
 
     }
 
